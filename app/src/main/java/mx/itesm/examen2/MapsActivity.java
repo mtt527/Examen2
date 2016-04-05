448117b9998fa714a942946da54da4cc4de6e184
victor eduardo <victor eduardo>
2016-04-05 11:35:34 -0500

    Examen parcial 2, uso de mapas y GPS

---
+++ app/src/main/java/mx/itesm/examen2/MapsActivity.java
@@ -0,0 +1,216 @@
+package mx.itesm.examen2;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.security.Provider;
+import java.util.List;
+import java.util.Locale;
+
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    private  static Context context;
+
+    LocationManager locationManager;
+    String provider;
+
+    MyLocationListener locationListener;
+
+    Double latitude;
+    Double longitude;
+
+    /*public void returnMenu(View view)
+    {
+
+        SharedPreferences locationData = getSharedPreferences("Location_Data", 0);
+        SharedPreferences.Editor locationDataEditor = locationData.edit();
+
+        String sLat = latitude.toString();
+        String sLon = longitude.toString();
+
+        locationDataEditor.putString("Latitude", sLat);
+        locationDataEditor.commit();
+
+        locationDataEditor.putString("Longitude", sLon);
+        locationDataEditor.commit();
+
+
+        startActivity(new Intent(this, MainActivity.class));
+        //this.finish();
+    }*/
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        SharedPreferences locationData = getSharedPreferences("Location_Data", 0);
+
+        latitude = Double.parseDouble(locationData.getString("Latitud", "0"));
+        longitude = Double.parseDouble(locationData.getString("Longitud", "0"));
+
+        /*mMap.clear();
+        mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("Your Position"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), 10));*/
+
+        context = getApplicationContext();
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        provider = locationManager.getBestProvider(new Criteria(), false);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+        locationListener = new MyLocationListener();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(provider, 400, 1, locationListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.removeUpdates(locationListener);
+    }
+
+    public class MyLocationListener extends Activity implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            Geocoder geocoder = new Geocoder(getContext(), Locale.getDefault());
+            Double lat = location.getLatitude();
+            Double lon = location.getLongitude();
+            Double alt = location.getAltitude();
+
+            Float acu = location.getAccuracy();
+            Float spd = location.getSpeed();
+            Float bea = location.getBearing();
+
+            latitude = lat;
+            longitude = lon;
+            Log.i("Latitude>> ", lat.toString());
+            Log.i("Longitude>> ", lon.toString());
+            Log.i("Altitude>> ", alt.toString());
+            Log.i("Acuracy>> ", acu.toString());
+            Log.i("Speed>> ", spd.toString());
+            Log.i("Bearing>> ", bea.toString());
+
+            try{
+                List<Address> listAdresses = geocoder.getFromLocation(lat,lon,1);
+                if((listAdresses != null) && (listAdresses.size() > 0))
+                {
+                    Log.i("Adress>> ", listAdresses.get(0).toString());
+                }
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+
+            mMap.clear();
+            mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lon)).title("Your Position"));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lon), 10));
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    }
+
+    public static Context getContext()
+    {
+        return context;
+    }
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng user = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(user).title("Your Position"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(user,10));
+    }
+}
+
+
